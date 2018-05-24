4fbb16508273571632ac669b2fee16eae3120c31
Sudharshan <sudhar393@gmail.com>
2018-05-24 20:42:29 +0800

    added basic logic for opening a port and appropriate error checking

--- python-driver/driver.py
+++ python-driver/driver.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python2
+import sys
 import pyautogui
 import serial
 
 def main():
-    print "Hello World"
+    # Serialport open
+    ser = serial.Serial()
+    ser.baudrate = 115200
 
-    for i in range (5):
-        pyautogui.press('a');
+    # Attempt accessing argument
+    try:
+        ser.port = sys.argv[1]
+    except IndexError:
+        print 'You did not specify a port'
+        return
+
+    ser.timeout = 1000
+
+    # Attempt opening port
+    try:
+        ser.open()
+    except serial.serialutil.SerialException:
+        print 'Port must be valid'
+        return
 
 if __name__ == "__main__":
     main()
