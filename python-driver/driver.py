7ff21dd57a5de393ad9b09e1d57f0c071cee583b
Sudharshan <sudhar393@gmail.com>
2018-05-25 09:36:09 +0800

    added generater for serial

--- python-driver/driver.py
+++ python-driver/driver.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python2
 import sys
 import pyautogui
-import serial
+import seriali
 
-def main():
-    # Serialport open
+def serial_data(baudrate):
     ser = serial.Serial()
-    ser.baudrate = 115200
+    ser.baudrate = baudrate
 
-    # Attempt accessing argument
     try:
         ser.port = sys.argv[1]
     except IndexError:
@@ -17,12 +15,21 @@ def main():
 
     ser.timeout = 1000
 
-    # Attempt opening port
     try:
         ser.open()
     except serial.serialutil.SerialException:
-        print 'Port must be valid'
+        print 'Invalid port'
         return
+
+    while True:
+        yield ser.readline()
+
+    ser.close()
+
+
+def main():
+    for packet in serial_data(115200):
+        print packet;
 
 if __name__ == "__main__":
     main()
